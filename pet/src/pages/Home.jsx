a3cad84783105945b77292659cbe13d5bfd894ac
Juan-code999 <juancharlesasimoes08@gmail.com>
2025-05-16 17:07:17 -0300

    home

--- pet/src/pages/Home.jsx
+++ pet/src/pages/Home.jsx
@@ -1,132 +1,86 @@
-import React, { useRef } from "react";
-import "../styles/Home.css";
-import { Link } from "react-router-dom";
+import React from 'react';
+import '../styles/Home.css';
+import dog1 from '../img/dog2.png';
+import dog2 from '../img/dog2.png';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  FaRocket,
-  FaClipboardList,
-  FaDog,
-  FaShieldAlt,
-  FaBath,
-  FaHeart,
-  FaSyringe,
-  FaArrowLeft,
-  FaArrowRight
-} from "react-icons/fa";
+  faHandPaper,
+  faDog,
+  faPaw,
+  faLeaf
+} from '@fortawesome/free-solid-svg-icons';
 
-const Home = () => {
-  const scrollRef = useRef(null);
-
-  const scrollLeft = () => {
-    scrollRef.current.scrollBy({ left: -320, behavior: "smooth" });
-  };
-
-  const scrollRight = () => {
-    scrollRef.current.scrollBy({ left: 320, behavior: "smooth" });
-  };
-
+export default function Home() {
   return (
-    <div className="home-container">
-      {/* Hero Section */}
-      <div className="hero-section">
-        <div className="hero-overlay">
-          <div className="hero-text">
-            <h1>Cuidado, Carinho e Confiança para seu Pet</h1>
-            <p>
-              Oferecemos banho, tosa e serviços personalizados para deixar seu pet sempre feliz e saudável.
-            </p>
-            <div className="cta-buttons">
-              <Link className="cta-button" to="/agendar">
-                <FaRocket className="button-icon" />
-                Agendar Serviço
-              </Link>
-              <Link className="cta-button secondary" to="/servicos">
-                <FaClipboardList className="button-icon" />
-                Ver Serviços
-              </Link>
+    <div className="home">
+      {/* HEADER / HERO */}
+      <header className="header">
+        <div className="header-text">
+          <h1>We are daying dog<br />traint to 1 ferfms.</h1>
+          <p>
+            Consciduving Trakuser pod stershupo ent diabi grom anq Gosndlefen ers.<br />
+            Ors ssp y allwore you brennutmyerungrof rutetclners.
+          </p>
+          <button className="btn-primary">Now Reciph</button>
+        </div>
+      </header>
+
+      {/* SERVICES / CARDS */}
+      <section className="cards-section">
+        <h2>Wat us all for ours</h2>
+        <div className="cards-container">
+          <div className="card">
+            <div className="icon-circle">
+              <FontAwesomeIcon icon={faHandPaper} />
             </div>
+            <h3>Rethet</h3>
+            <p>Dasdt lkmolene gosldu etf beac smprsk rgogin</p>
+            <a href="#!">Sh yvks</a>
+          </div>
+          <div className="card">
+            <div className="icon-circle">
+              <FontAwesomeIcon icon={faDog} />
+            </div>
+            <h3>Bzens</h3>
+            <p>Grasq o selekltembs yel hoernntay et sit Senpra</p>
+            <a href="#!">Sum daq rnestlq</a>
+          </div>
+          <div className="card">
+            <div className="icon-circle">
+              <FontAwesomeIcon icon={faPaw} />
+            </div>
+            <h3>Corvlinge</h3>
+            <p>Raia poprkolsn orfrortos entlenet vel orrenae.</p>
+            <a href="#!">Dres led on poprilis</a>
+          </div>
+          <div className="card">
+            <div className="icon-circle">
+              <FontAwesomeIcon icon={faLeaf} />
+            </div>
+            <h3>Det leam</h3>
+            <p>Loqed evrahlle eno popraete fromt qod.</p>
+            <a href="#!">Fos duto &amp; drugs</a>
           </div>
         </div>
-      </div>
+      </section>
 
-      {/* Cuidados Section */}
-      <div className="cuidados">
-        <h2 className="titulo">Cuidados com seu Pet</h2>
-        <div className="nav-btns">
-          <button className="btn-nav" onClick={scrollLeft}>
-            <FaArrowLeft />
-          </button>
-          <button className="btn-nav purple" onClick={scrollRight}>
-            <FaArrowRight />
-          </button>
+      {/* DOGS INFO */}
+      <section className="dogs-section">
+        <div className="dogs-text">
+          <h2>Worit leorins time<br /><span>on hale it hon.</span></h2>
+          <p>
+            Thse pot vvil resurtirg seng asng lhtu hos bones. Tascer Dra tveand psut ylf der luctas f rowiradnehq, en finare sitent bysluew dsnong onwutpt hhe, beasj ntroh lutrs of onep sel to reetla in the pot.
+          </p>
+          <p>
+            Fedakhad tas tas sowneve lquchvthrlarub trba esndlqp tust temq Etap croe of cpnadt ondedfanson) citl poy netan ovis si duwon oad od enylt srooloyqeler.
+          </p>
+          <button className="btn-primary">Consulting Nda</button>
         </div>
-
-        <div className="cards-container" ref={scrollRef}>
-          <div className="card">
-            <div className="cabecalho">
-              <FaBath />
-              Higiene
-            </div>
-            <div className="card-title">Banhos Regulares</div>
-            <div className="descricao">Mantenha seu pet limpinho com banhos especiais para cada tipo de pelagem.</div>
-            <button className="btn-agendar">Agendar Banho</button>
-            <button className="btn-saiba">Saiba Mais</button>
-          </div>
-
-          <div className="card">
-            <div className="cabecalho">
-              <FaDog />
-              Tosa
-            </div>
-            <div className="card-title">Tosa com estilo</div>
-            <div className="descricao">Tosa higiênica, na tesoura ou estilizada para manter seu pet confortável e lindo.</div>
-            <button className="btn-agendar">Agendar Tosa</button>
-            <button className="btn-saiba">Saiba Mais</button>
-          </div>
-
-          <div className="card">
-            <div className="cabecalho">
-              <FaShieldAlt />
-              Proteção
-            </div>
-            <div className="card-title">Produtos de Qualidade</div>
-            <div className="descricao">Usamos shampoos, perfumes e produtos dermatologicamente testados e seguros.</div>
-            <button className="btn-agendar">Ver Produtos</button>
-            <button className="btn-saiba">Saiba Mais</button>
-          </div>
-
-          <div className="card">
-            <div className="cabecalho">
-              <FaSyringe />
-              Vacinação
-            </div>
-            <div className="card-title">Vacinas em dia</div>
-            <div className="descricao">Manter a vacinação em dia é essencial para a saúde do seu pet.</div>
-            <button className="btn-agendar">Ver Calendário</button>
-            <button className="btn-saiba">Saiba Mais</button>
-          </div>
-
-          <div className="card">
-            <div className="cabecalho">
-              <FaHeart />
-              Bem-estar
-            </div>
-            <div className="card-title">Amor e atenção</div>
-            <div className="descricao">Cuidamos do seu pet como se fosse da nossa família, com carinho e responsabilidade.</div>
-            <button className="btn-agendar">Agendar Visita</button>
-            <button className="btn-saiba">Saiba Mais</button>
-          </div>
+        <div className="dogs-images">
+          <img src={dog1} alt="Dog 1" />
         </div>
-
-        {/* Botão Agendamentos */}
-        <div className="btn-agendamentos-wrapper">
-          <Link to="/agendamentos" className="btn-agendamentos">
-            Ver Agendamentos
-          </Link>
-        </div>
-        
-      </div>
+      </section>
     </div>
   );
-};
-
-export default Home;
+}
